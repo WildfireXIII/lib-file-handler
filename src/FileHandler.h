943bda2c8114af4d0224a48244fed2707dac64a8
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-01-13 16:46:42 -0500

    Implemented windows side of first few path functions

--- src/FileHandler.h
+++ src/FileHandler.h
@@ -1,5 +1,5 @@
 //*************************************************************
-//  File: 
+//  File: FileHandler.h
 //  Date created: 1/13/2016
 //  Date edited: 1/13/2016
 //  Author: Nathan Martindale
@@ -7,31 +7,56 @@
 //  Description: Cross-platform file handling library
 //*************************************************************
 
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+
+#include <iostream>
 #include <string>
+#include <vector>
 
 #ifdef _WIN32
-#endif
+	#include <windows.h>
+#endif // _WIN32
 
 #ifdef linux
-#endif
+#endif // linux
 
+using namespace std;
 
 namespace dwl
 {
 	struct FileListing
 	{
-		string FullName; //full name, includes extension (if file)
+		string FullName; // full name, includes extension (if file)
 		string Name;
-		String Path;
-		String Extension;
+		string Path;
+		string Extension;
 		bool isDirectory;
 	};
 	
 	class FileHandler
 	{
-	private:
+		private:
+			string m_currentPath;
 		
-	public:
-	
-	}
+		public:
+			FileHandler();
+			~FileHandler(); // TODO: don't forget to close any open files here
+
+			// path/directory stuff
+			string getCWD(); 
+			string getApplicationPath();
+			/*string getCurrentPath();
+			void setCurrentPath(string path); // NOTE: only sets virtual path
+			vector<FileListing> getDirectoryListing();
+
+			// file i/o
+			vector<string> readFile(string fileName);
+			void prepareFileWrite(string fileName, int mode);
+			void fileWrite(string content);
+			void finishWrite();
+			void quickWrite(string content, string fileName, int mode); */
+	};
 }
+
+#endif // FILE_HANDLER_H
